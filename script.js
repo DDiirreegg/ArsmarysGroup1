1a4c0a959f2458a056c140d7c4bddc8b323778b0
DDiirreegg <arseniy.5444@gmail.com>
2024-03-13 21:37:17 +0100

    addfirstcommit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleDescription(element) {
+  document.getElementById(element).classList.toggle('show-info');
+}
+
+
+function toggleAdditionalInfo(index) {
+  var additionalInfo = document.getElementById('additional-info-' + index);
+  var arrow = document.querySelector('#heading-' + index + ' .arrow');
+  
+  if (additionalInfo.style.display === 'none' || additionalInfo.style.display === '') {
+      additionalInfo.style.display = 'block';
+      additionalInfo.style.opacity = '1'; // добавляем свойство opacity, чтобы блок был видимым
+      document.getElementById('heading-' + index).classList.add('open'); // Добавлено присваивание класса .open
+  } else {
+      additionalInfo.style.display = 'none';
+      additionalInfo.style.opacity = '0'; // добавляем свойство opacity, чтобы блок был скрытым
+      document.getElementById('heading-' + index).classList.remove('open'); // Добавлено удаление класса .open
+  }
+}
